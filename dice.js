dfbe030ea46e67bd90cd8b2c1ad309841d2af7be
prashantpatil2002 <prashantpatil29498@gmail.com>
2020-05-16 20:12:42 +0530

    dice
    game

--- dice.js
+++ dice.js
@@ -1,9 +1,9 @@
  
 var random1 = Math.floor(Math.random()*6)+1;
-var image = "/images/dice" + random1 +".png";
+var image = "dice" + random1 +".png";
 var select = document.querySelectorAll("img")[0].setAttribute("src",image);
 var random2 = Math.floor(Math.random()*6)+1;
-var image1 = "/images/dice"+ random2 + ".png";
+var image1 = "dice"+ random2 + ".png";
 var select1 = document.querySelectorAll("img")[1].setAttribute("src",image1);
 if(random1>random2){
     document.getElementById("h").textContent = "PLAYER   1   WINS!!";
